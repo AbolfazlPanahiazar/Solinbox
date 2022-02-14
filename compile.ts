f9cf254fae208121a74f37af1e1f8307efff70bd
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2022-02-14 04:14:09 +0330

    Add compile script

---
+++ compile.ts
@@ -0,0 +1,24 @@
+import * as path from "path";
+import * as fs from "fs";
+const solc = require("solc");
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf-8");
+
+const input = {
+  language: "Solidity",
+  sources: {
+    "Inbox.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+console.log(solc.compile(JSON.stringify(input)));
---
+++ index.d.ts
@@ -0,0 +1,2 @@
+declare module "solc";
+export {};
