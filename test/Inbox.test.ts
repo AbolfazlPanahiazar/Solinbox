1c014c00582a6770e235e10cd93f27181c12ba1f
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2022-02-19 13:45:07 -0500

    set up mocha

--- test/Inbox.test.ts
+++ test/Inbox.test.ts
@@ -1,3 +1,23 @@
 import assert from "assert";
-import ganache from "ganache-cli";
-import Web3 from "web3";
+// import ganache from "ganache";
+// import Web3 from "web3";
+import { describe, it } from "mocha";
+
+// const web3 = new Web3(ganache.provider());
+
+class Car {
+  park() {
+    return "stopped";
+  }
+
+  drive() {
+    return "vroom";
+  }
+}
+
+describe("Car", () => {
+  it("can park", () => {
+    const car = new Car();
+    assert.equal(car.park(), "stopped");
+  });
+});
