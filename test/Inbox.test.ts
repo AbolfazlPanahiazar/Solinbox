58aaec0967a96753b3901ddf062e3c54c2c965fe
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2022-02-20 10:35:45 -0500

    Add contract setMessage test

--- test/Inbox.test.ts
+++ test/Inbox.test.ts
@@ -7,25 +7,37 @@ import { beforeEach, describe, it } from "mocha";
 import { abi, evm } from "../compile";
 import { Contract } from "web3-eth-contract";
 
-console.log("abi", abi);
-
 // @ts-ignore
 const web3 = new Web3("ws://localhost:8545");
 
 let accounts: string[];
 let inbox: Contract;
+const INITIAL_STRING = "Hi there!";
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   inbox = await new web3.eth.Contract(abi)
     .deploy({
       data: evm.bytecode.object,
-      arguments: ["Hi there!"],
+      arguments: [INITIAL_STRING],
     })
     .send({ from: accounts[0], gas: 1000000 });
 });
 
 describe("Inbox", () => {
   it("deployes a contract", () => {
-    console.log(abi);
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.messageText().call();
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it("it can change the message", async () => {
+    const transaction = await inbox.methods
+      .setMessageText("Bye there!")
+      .send({ from: accounts[0] });
+    const message = await inbox.methods.messageText().call();
+    assert.equal(message, "Bye there!");
   });
 });
